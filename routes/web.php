551bc40eec703bbf1a2d0876f835ec125ba91d41
Héctor Javier Ordóñez García <hector.javier.ordonez@awareways.com>
2025-01-19 17:15:23 +0100

    added simple wip landing page for products

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('products/store', \App\Http\Controllers\ProductsController::class . '@store')->name('products.store');
