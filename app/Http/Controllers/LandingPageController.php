7f7688b54e2892c3562fc8b0867a3f4243c323e1
Héctor Javier Ordóñez García <hector.javier.ordonez@awareways.com>
2025-01-19 17:21:54 +0100

    added landing page controller and removed direct link to view

---
+++ app/Http/Controllers/LandingPageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class LandingPageController extends Controller
+{
+    public function index()
+    {
+        return view('landing-page.index');
+    }
+    public function store()
+    {
+        dd('Storing!');
+    }
+}
